f8f57c3504d9061e2817b63b0fea03c1a3400f6c
Sebiisgarb <seby3pop.ps10@gmail.com>
2025-01-15 19:39:51 +0200

    workouts app

---
+++ Backend/fittrack/workouts/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Workout(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    duration = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+class Exercise(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    muscle_group = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class WorkoutExercise(models.Model):
+    workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    sets = models.IntegerField()
+    reps = models.IntegerField()
+    weight = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.workout.name} - {self.exercise.name}'
+    
+class UserWorkout(models.Model):
+    user = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE)
+    workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.workout.name}'
