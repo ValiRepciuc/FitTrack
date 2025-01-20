8f4aebc9806c1f2b87c1c9b1d4da204d9e77451f
Vali Repciuc <valirepciuc@Valis-MacBook-Pro.local>
2025-01-20 18:20:45 +0200

    RefreshTokenWorks

--- Backend/fittrack/fittrack/urls.py
+++ Backend/fittrack/fittrack/urls.py
@@ -19,12 +19,14 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/workouts/', include('workouts.urls')),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 if settings.DEBUG:
